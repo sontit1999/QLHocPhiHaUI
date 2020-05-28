7bbfbfdf8a127528edb53f2dc03e41eb81a51f34
son tit <sonkoii1995@gmail.com>
2020-05-28 19:04:44 +0700

    done modul DSSV and Nhaphoc

--- src/controller/SinhVienController.java
+++ src/controller/SinhVienController.java
@@ -5,7 +5,7 @@
  */
 package controller;
 
-import com.mysql.cj.protocol.Resultset;
+import database.Connect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import model.SinhVien;
@@ -25,73 +26,69 @@ import utils.Constant;
  * @author quyen
  */
 public class SinhVienController {
+
+    Connect connect = new Connect();
     public static String USERNAME = "root";
     public static String PASSWORD = "";
     public static String DATABASEURL = "jdbc:mysql://localhost:3306/thuhocphisv";
     public static String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+
+    public SinhVienController() {
+    }
+
     public static void themSinhVien(SinhVien sv) {
 
     }
 
-    public static void themLop() {
-
+    public int deleteSV(String masv) {
+         String sqlDelete = "delete from sinhvien where masv = " + masv;
+         if(connect.UpdateData(sqlDelete)==1){
+             return 1;
+         }else{
+              return 0;
+         }    
     }
-
-    public static void getSV(String masv, JTable table) {
+  
+    public void getSV(String masv, JTable table) {
+        masv = masv.trim();
         List<SinhVien> list = new ArrayList<>();
+        list.clear();
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0);
-        Connection connection = null;
-        Statement statement = null;
-        
+
+        ResultSet resultset = connect.queryData("select * from sinhvien");
         try {
-            Class.forName(jdbcDriver);
-            System.out.println("Đang kết nối csdl");
-            connection = DriverManager.getConnection(DATABASEURL,USERNAME,PASSWORD);
-            if(connection!=null){
-                System.out.println("Connect thành công");
-            }else{
-               System.out.println("Connect fail");
+            while (resultset.next()) {
+
+                SinhVien sv = new SinhVien(resultset.getString("masv"), resultset.getString("hoten"), resultset.getString("ngaysinh"), resultset.getString("gioitinh"), resultset.getString("quequan"), resultset.getString("sdt"), resultset.getString("gmail"), resultset.getString("malop"), resultset.getInt("congno") + "");
+                list.add(sv);
+
             }
-            // truy vấn
-            statement = connection.createStatement();
-            String sql = "select * from sinhvien";
-            ResultSet result = statement.executeQuery(sql);
-            
-            
-            
-            
-            /*
-            if (masv.equals("")) {
-            for (SinhVien i : list) {
-            Object[] row = {i.getMasv(), i.getHoten(), i.getNgaysinh(), i.getGioitinh(), i.getQuequan(), i.getSdt(), i.getGmail(), i.getMalop(), i.getCongno()};
-            model.addRow(row);
-            };
-            } else {
-            for (SinhVien i : list) {
-            if (masv.trim().equalsIgnoreCase(i.getMasv())) {
-            Object[] row = {i.getMasv(), i.getHoten(), i.getNgaysinh(), i.getGioitinh(), i.getQuequan(), i.getSdt(), i.getGmail(), i.getMalop(), i.getCongno()};
-            model.addRow(row);
-            break;
-            };
-            }
-            }
-            
-            // lấy tất dssv
-            /*
-            list.add(new SinhVien("SV01", "Son tit 1", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            list.add(new SinhVien("SV02", "Son tit 2", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            list.add(new SinhVien("SV03", "Son tit 3", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            list.add(new SinhVien("SV04", "Son tit 4", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            list.add(new SinhVien("SV05", "Son tit 5", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            list.add(new SinhVien("SV06", "Son tit 6", "06/10/1999", "Nam", "Ha Tay", "0335275330", "tvson999@gmail.com", "L001", "0"));
-            */
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(SinhVienController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(SinhVienController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        if (masv.equals("")) {
+            for (SinhVien i : list) {
+
+                Object[] row = {i.getMasv(), i.getHoten(), i.getNgaysinh(), i.getGioitinh(), i.getQuequan(), i.getSdt(), i.getGmail(), i.getMalop(), i.getCongno()};
+                model.addRow(row);
+
+            }
+        }else{
+             boolean have = false;
+             for (SinhVien i : list) {
+                if(masv.equalsIgnoreCase(i.getMasv())){
+                     Object[] row = {i.getMasv(), i.getHoten(), i.getNgaysinh(), i.getGioitinh(), i.getQuequan(), i.getSdt(), i.getGmail(), i.getMalop(), i.getCongno()};
+                     model.addRow(row);
+                     have = true;
+                     break;
+                }
+               
+            }
+            if(!have){
+             JOptionPane.showMessageDialog(table, " Không tìm thấy  sinh viên có mã " + masv);
+            }
+        }
 
     }
 }
